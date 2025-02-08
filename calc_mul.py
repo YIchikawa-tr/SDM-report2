c802443caa19c6bbf191dea2d96cac582f8b68db
YIchikawa <ichikawa.yuto.eb@tut.jp>
2025-02-08 09:45:43 +0900

    fix:calc_num.py

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,13 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
+#       p = re.compile('\d+(\.\d+)?')
+        p = re.compile('^\d+$') # changed
         if p.match(ai) or p.match(bi):
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+#               if 0<a and a<b and b<1000:
+                if 0<a<1000 and 0<b<1000: # changed
                         valid=True
                 else:
                         valid=False
